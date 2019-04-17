8eb6a3d4512e16da9d8c1b13719c6684d54907ed
KaMi lml <kami@dotastore.pe>
2019-04-16 21:43:39 -0500

    quitando archivos innecesarios

--- config.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-module.exports = {
-    usernameSUNAT: '20100066603MODDATOS',
-    passwordSUNAT: 'moddatos',
-
-    billServiceUriSUNAT: 'https://e-beta.sunat.gob.pe/ol-ti-itcpfegem-beta/billService'
-}
